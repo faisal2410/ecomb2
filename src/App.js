5056518c3861d7e7a8c4b2b26ddae19b12c15eb8
Faisal ahmed <faisal2410@yahoo.com>
2023-02-08 20:59:31 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Menu from "./components/nav/Menu";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
+const  App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
+    
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import Jumbotron from "../components/cards/Jumbotron";
+
+const  Login=()=> {
+  return (
+    <div>
+      <Jumbotron title="Login" />
+    </div>
+  );
+}
+
+export default Login;
